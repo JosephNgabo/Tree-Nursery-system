5e5fa43fed697f21b184c0e9e04325d394aa64e8
JosephNgabo <joseph.ntwali@adfinance.co>
2025-04-24 11:00:25 +0200

    new changes

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -1,6 +1,7 @@
 // swagger.js
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const isProduction = process.env.NODE_ENV === 'production';
 
 const options = {
   definition: {
